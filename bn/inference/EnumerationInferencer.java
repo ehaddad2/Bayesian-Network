87b952799e650737ed8f5f6384e662c26424726a
ehaddad2 <ehaddad2@u.rochester.edu>
2023-11-01 17:15:11 -0400

    examples and exact enumeration class added

---
+++ bn/inference/EnumerationInferencer.java
@@ -0,0 +1,14 @@
+package bn.inference;
+
+import bn.core.*;
+
+public class EnumerationInferencer {
+    
+    public EnumerationInferencer() {
+
+    }
+
+    public EnumerationInferencer query (RandomVariable a, Assignment e, BayesianNetwork b) {
+        return new EnumerationInferencer(); //TODO: CHANGE!!!
+    }
+}
