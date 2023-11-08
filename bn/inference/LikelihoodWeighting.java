220dd54ce07dbd405c414d4fd63e6a95185d9de8
asthasingh03 <asingh50@u.rochester.edu>
2023-11-07 19:31:47 -0500

    new main thing

--- bn/inference/LikelihoodWeighting.java
+++ bn/inference/LikelihoodWeighting.java
@@ -9,79 +9,87 @@ import java.util.List;
 import java.util.Random;
 
 
-public class LikelihoodWeighting 
-{
-    private int N;
 
-    public LikelihoodWeighting(int N) {
-        this.N = N;
-    }
 
-    public static class weight {
-        Assignment e;
-        double weight;
-        weight(Assignment e, double weight) {
-            this.e = e;
-            this.weight = weight;
-        }
-    }
+public class LikelihoodWeighting {
+   private int N;
 
-    // Sample an event from bn that's consistent with the evidence e;
-    // return the event and its weight, the likelihood that the event
-    // accords to the evidence.
 
-    public static weight weightedSample(BayesianNetwork bn, Assignment e) {
-        double w = 1.0;
-        Assignment x = e.copy();
+   public LikelihoodWeighting(int N) {
+       this.N = N;
+   }
 
-        //get all random variables in bn
-        List<RandomVariable> rvs = bn.getVariablesSortedTopologically();
 
-        for (RandomVariable rand : rvs) {
+   public static class weight {
+       Assignment e;
+       double weight;
+       weight(Assignment e, double weight) {
+           this.e = e;
+           this.weight = weight;
+       }
+   }
 
-            if (e.containsKey(rand)) {
-                x.put(rand, e.get(rand));
-                w *= bn.getProbability(rand, x);
-			} 
 
-            else {
-				// to get a random variable between 0 and 1
-				Random random = new java.util.Random();
-				double randnum = random.nextDouble();
-							
-				Range Y = rand.getRange();
-				// for each value in RV's domain
-				for (Value val : Y) {	
-					x.put(rand, val);
-					randnum -= bn.getProbability(rand, x);
-					if (randnum <= 0) {
-						break;
-                    }
-                }
-            }  
-        }
-        return new weight(x, w);
-    }
+   public static weight weightedSample(BayesianNetwork bn, Assignment e) {
+       double w = 1.0;
+       Assignment x = e.copy();
 
-    public Distribution query(RandomVariable X, Assignment e, BayesianNetwork bn) {
-        
-        //store counts for each value
-        Distribution dist = new Distribution(X);
-        for (Value val : X.getRange()) {
-            dist.set(val, 0.0);
-        }
-        
-        
-        for(int j = 1; j <= N; j++) {
-            weight wa = weightedSample(bn, e);
-            Assignment tempAssignment = wa.e;
-            double w = wa.weight;
 
-            dist.put(tempAssignment.get(X), dist.get(tempAssignment.get(X)) + w);
-        }
-        dist.normalize();
-        return dist;
-    }
+       //get all random variables in bn
+       List<RandomVariable> rvs = bn.getVariablesSortedTopologically();
+
+
+       for (RandomVariable rand : rvs) {
+
+
+           if (e.containsKey(rand)) {
+               x.put(rand, e.get(rand));
+               w *= bn.getProbability(rand, x);
+           }
+
+
+           else {
+
+
+               // random variable between 0 and 1
+               Random random = new java.util.Random();
+               double randnum = random.nextDouble();
+                          
+               Range Y = rand.getRange();
+
+
+               for (Value val : Y) {  
+                   x.put(rand, val);
+                   randnum -= bn.getProbability(rand, x);
+                   if (randnum <= 0) {
+                       break;
+                   }
+               }
+           } 
+       }
+       return new weight(x, w);
+   }
+
+
+   public Distribution query(RandomVariable X, Assignment e, BayesianNetwork bn) {
+      
+       Distribution dist = new Distribution(X);
+       for (Value val : X.getRange()) {
+           dist.set(val, 0.0);
+       }
+      
+      
+       for(int j = 1; j <= N; j++) {
+           weight wa = weightedSample(bn, e);
+           Assignment tempAssignment = wa.e;
+           double w = wa.weight;
+
+
+           dist.put(tempAssignment.get(X), dist.get(tempAssignment.get(X)) + w);
+       }
+       dist.normalize();
+       return dist;
+   }
+
 
 }
-
---
+++ main/test.java
@@ -0,0 +1,116 @@
+package main;
+
+import java.lang.String;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+import bn.core.*;
+import bn.base.Assignment;
+import bn.base.StringValue;
+import bn.inference.EnumerationInferencer;
+import bn.inference.LikelihoodWeighting;
+import bn.inference.RSampleInferencer;
+import bn.parser.XMLBIFParser;
+import bn.core.Value;
+
+
+public class test {
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        XMLBIFParser parser = new XMLBIFParser();
+
+        try {
+            String filename;
+            if (args.length > 0) {
+                filename = args[0];
+            } else {
+                System.out.println("Which type of example do you want to run? Enter the path to the XML/BIF file:");
+                filename = reader.readLine();
+            }
+            
+            BayesianNetwork network = parser.readNetworkFromFile(filename);
+            System.out.println("Bayesian Network loaded:");
+            System.out.println(network);
+
+            // Ask for the query variable
+            System.out.println("Enter the query variable (e.g., 'A'):");
+            String queryVariableName = reader.readLine();
+            RandomVariable queryVariable = network.getVariableByName(queryVariableName);
+
+            // Ask for evidence
+            Assignment evidence = new Assignment();
+            System.out.println("Enter evidence (case sensitive) (e.g., 'B,true'). Enter 'done' when finished:");
+            String line;
+            while (!(line = reader.readLine()).equals("done")) {
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    RandomVariable eVar = network.getVariableByName(parts[0]);
+                    StringValue s = new StringValue(parts[1]);
+                    evidence.put(eVar, (Value)s);
+                } else {
+                    System.out.println("Invalid evidence format. Please try again.");
+                }
+            }
+
+            // ask user which inferencer to use
+            System.out.println("\n");
+            System.out.println("Choose inferencer type (enumeration, rejection, likelihood):):");
+            System.out.println("1 - Enumeration Inferencer");
+            System.out.println("2 - Rejection Sampling");
+            System.out.println("3 - Likelihood Weighting");
+            System.out.println("0 - Exit");
+
+            Distribution distribution = null;
+
+            int inferencerType = Integer.parseInt(reader.readLine());
+
+            switch (inferencerType) {
+                case 1:
+                    EnumerationInferencer eInferencer = new EnumerationInferencer();
+                    distribution = eInferencer.query(queryVariable, evidence, network);
+                    break;
+                case 2:
+                    System.out.println("Enter sample size for Rejection Sampling:");
+                    int sampleSize = Integer.parseInt(reader.readLine());
+                    RSampleInferencer rsInferencer = new RSampleInferencer(sampleSize);
+                    distribution = rsInferencer.query(queryVariable, evidence, network);
+                    break;
+                case 3:
+                    System.out.println("Enter sample size for Likelihood Weighting:");
+                    int n = Integer.parseInt(reader.readLine());
+                    LikelihoodWeighting lwInferencer = new LikelihoodWeighting(n);
+                    distribution = lwInferencer.query(queryVariable, evidence, network);
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                    return; //exit the program
+                default:
+                    throw new IllegalArgumentException("Invalid choice of inferencer.");
+            }
+
+            // Output the result
+            if (distribution != null) {
+                System.out.println("Computed distribution for " + queryVariableName + ":");
+                for (Object value : distribution.keySet()) {
+                    System.out.println(value + ": " + distribution.get(value));
+                }
+            } else {
+                System.out.println("No inferencing performed.");
+            }
+        
+        } catch (IOException e) {
+            System.out.println("IO EXCEPTION ENCOUNTERED, Message: " + e.getMessage());
+        } catch (ParserConfigurationException p) {
+            System.out.println("PARSER EXCEPTION ENCOUNTERED, Message: " + p.getMessage());
+        } catch (SAXException s) {
+            System.out.println("SAX EXCEPTION ENCOUNTERED, Message: " + s.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
